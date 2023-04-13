bbe85a806bad74755c8a94ca56b5c3e03eea4244
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2023-04-13 15:07:49 +0200

    update

--- bot/index.js
+++ bot/index.js
@@ -3,6 +3,7 @@ const {
   Client,
   GatewayIntentBits,
   SlashCommandBuilder,
+  EmbedBuilder,
 } = require("discord.js");
 client = new Client({
   intents: [
@@ -35,12 +36,81 @@ client.once("ready", () => {
   );
 });
 
-client.on("interactionCreate", (interaction) => {
+client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
     console.log("here");
     if (interaction.commandName === "petition") {
-      let sujet = interaction.options.getString("sujet");
-      interaction.reply(`la pétition pour le sujet ${sujet} a été crée`);
+      let question = interaction.options.getString("sujet");
+      let choices = ["oui", "non"];
+      const results = {};
+
+      // Crée un message avec la question et les choix de réponses
+      const pollMessage = await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle(question)
+            .setDescription(
+              choices
+                .map((choice, index) => `${index + 1}. ${choice}`)
+                .join("\n")
+            )
+            .setColor(0x0099ff),
+        ],
+        fetchReply: true,
+      });
+
+      // Ajoute des réactions pour chaque choix de réponse
+      for (let i = 0; i < choices.length; i++) {
+        await pollMessage.react(`${i + 1}\u20E3`);
+      }
+
+      // Attend 10 secondes pour permettre aux utilisateurs de voter
+      const collector = pollMessage.createReactionCollector({
+        time: 10000,
+      });
+
+      // Enregistre les réponses des utilisateurs dans un objet
+      collector.on("collect", (reaction, user) => {
+        // Enregistre le choix de réponse de l'utilisateur
+        const choiceIndex = choices.findIndex(
+          (choice, index) => `${index + 1}\u20E3` === reaction.emoji.name
+        );
+
+        // Met à jour le nombre de votes pour ce choix de réponse
+        results[reaction.emoji.name] = (results[reaction.emoji.name] || 0) + 1;
+      });
+
+      // Affiche les résultats une fois le temps écoulé et envoie les votes à un serveur
+      collector.on("end", async () => {
+        const resultsEmbed = new EmbedBuilder()
+          .setTitle(question)
+          .setColor(0x00ff00)
+          .setDescription(
+            choices
+              .map(
+                (choice, index) =>
+                  `${choice}: ${results[`${index + 1}\u20E3`] || 0}`
+              )
+              .join("\n")
+          );
+        pollMessage.edit({ embeds: [resultsEmbed] });
+
+        // Envoie les votes à un serveur
+        const voteData = {
+          question,
+          choices,
+        };
+        console.log(voteData);
+        // const response = await fetch("http://example.com/votes", {
+        //   method: "POST",
+        //   body: JSON.stringify(voteData),
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        // });
+        // const responseData = await response.json();
+        // console.log(responseData);
+      });
     }
   }
 });
