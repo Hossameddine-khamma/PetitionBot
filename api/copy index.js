4a383108db2584a61c1459d41e1ce0de6c182286
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2023-04-21 16:03:27 +0200

    fix bug

--- api/copy index.js
+++ api/copy index.js
@@ -15,9 +15,10 @@ app.get("/health", async (req, res) => {
     if (client) {
       res.status(200).send("OK");
     }
-    client.close();
   } catch (err) {
     res.status(500).send("Error");
+  } finally {
+    await client.close();
   }
 });
 
@@ -38,11 +39,12 @@ app.post("/petition", async (req, res) => {
       const db = client.db("petition");
       const collection = db.collection("petition");
       const result = await collection.insertOne(petition);
-      client.close();
       return res.status(200).send(result);
     } catch (err) {
       console.log(err);
       return res.status(500).send("Catch", err);
+    } finally {
+      await client.close();
     }
   } else {
     return res.status(400).send("Bad request");
@@ -54,56 +56,66 @@ app.post("/petition/:id", async (req, res) => {
     try {
       await client.connect();
       const db = client.db("petition");
-      // console.log("====================================");
-      // console.log("here1");
-      // console.log("====================================");
       const collection = db.collection("petition");
-      console.log("====================================");
-      console.log(collection);
-      console.log("====================================");
       const query = { _id: new ObjectId(req.params.id) };
       const petition = await collection.findOne(query);
+
       if (petition) {
-        if (req.body.yes && req.body.no) {
-          var data = { $set: { yes: req.body.yes, no: req.body.no } };
-          const result = await collection.updateOne(
-            query,
-            data,
-            (err, collection) => {
-              if (err) console.log(err);
-              console.log("Record updated successfully");
-            }
-          );
-          client.close();
-          return res.status(200).send(result);
-        } else {
-          return res.status(400).send("Bad request");
-        }
+        return res.status(200).send(petition);
       } else {
         return res.status(404).send("Not found petition");
       }
+
+      // const result = await collection.insertOne(petition);
+      // return res.status(200).send(result);
     } catch (err) {
       console.log(err);
       return res.status(500).send("Catch", err);
+    } finally {
+      await client.close();
     }
   } else {
     return res.status(400).send("Bad request");
   }
+  // Document petition => user_create, sujet, date_create, date_close, id
+  // var date = new Date();
+  // const petition = {
+  //   user_create: req.body.username,
+  //   sujet: req.body.sujet,
+  //   date_create: new Date(),
+  //   date_close: new Date(date.setDate(date.getDate() + 7)),
+  // };
+
+  // try {
+  //   await client.connect();
+  //   const db = client.db("petition");
+  //   const collection = db.collection("petition");
+  //   const result = await collection.insertOne(petition);
+  //   return res.status(200).send(result);
+  // } catch (err) {
+  //   console.log(err);
+  //   return res.status(500).send("Catch", err);
+  // } finally {
+  //   await client.close();
+  // }
 });
 
 app.get("/petitions", async (req, res) => {
   try {
     await client.connect();
     const db = client.db("petition");
-    const collection = await db.collection("petition");
+    const collection = db.collection("petition");
     const result = await collection.find({}).toArray();
-    client.close();
     return res.status(200).send(result);
   } catch (err) {
     console.log(err);
-    return res.status(200).send({ message: "Petitions not found" });
+    return res.status(500).send("Catch", err);
+  } finally {
+    await client.close();
   }
 });
+
+app.get("/", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Start on port => ${port}`);
